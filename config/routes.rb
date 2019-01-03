6252f8b706fecb360f9dc5f82f32b6925d1dcc2c
Joel Peter <raphael3213@gmail.com>
2019-01-03 10:47:09 +0530

    new laptop

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  get 'session/new'
+
+ # get 'users/new'
+
+  root 'static_page#home'
+
+  get "help" => 'static_page#help'
+
+  get "about"=>'static_page#about'
+
+  get "signup" => "users#new"	
+  
+  get "login" =>"session#new"
+  
+  post "login" =>"session#create"
+  
+  delete "logout" =>"session#destroy"
+
+  
+	
+	resources:users
+ 
+end
